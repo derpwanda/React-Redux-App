b287361146f7019d7f59e02a01c75e31b881575b
derpWanda <derpWanda@gmail.com>
2019-10-24 19:31:17 -0500

    delete comments

--- hp_app/src/index.js
+++ hp_app/src/index.js
@@ -14,12 +14,5 @@ const store = createStore(combineReducers, composeWithDevTools(
 ));
 
 
-// const store = createStore(combineReducers,
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// );
-
-
-
-
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
